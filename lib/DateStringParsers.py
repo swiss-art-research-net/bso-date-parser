1c8226c4a3956bed61c06a656f5c891e8209d813
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-05 17:23:29 +0100

    order functions alphabetically

--- lib/DateStringParsers.py
+++ lib/DateStringParsers.py
@@ -1,16 +1,6 @@
 import re
 
 import lib.constants as constants
-
-def guessMonth(monthString):
-    testOrder = ['de', 'en', 'fr', 'roman']
-    monthString = re.sub(r'\.|\s', '', monthString)
-    for lang in testOrder:
-        for i in constants.MONTHTERMS[lang].keys():
-            for monthVariation in constants.MONTHTERMS[lang][i]:
-                if monthVariation.lower() == monthString.lower():
-                    return i
-    return 0
             
 def afterYearWithQualifier(dateString):
     yearSearch = re.search(r'(\d{4}\??)', dateString)
@@ -47,20 +37,6 @@ def century(dateString):
         return centuryEDTF + "?"
     else:
         return centuryEDTF
-    
-def midCentury(dateString):
-    centurySearch = re.search(r'(\d)\.\s?[A-zäöü]*\s?(\d{1,2})', dateString)
-    if not centurySearch:
-        return None
-    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
-    half = centurySearch.group(1)
-    century = centurySearch.group(2)
-    centuryEDTF = str(int(century)-1)
-    qualifier = '?' if uncertain else ''
-    if half == "1":
-        return century + "00" + qualifier + "/" + century + "50" + qualifier
-    else:
-        return century + "50" + qualifier + "/" + century + "99" + qualifier
 
 def centuryRange(dateString):
     centurySearch = re.findall(r'(\d{2})', dateString)
@@ -75,37 +51,6 @@ def centuryRange(dateString):
         return centuryFromEDTF + "?/" + centuryToEDTF + "?"
     else:
         return centuryFromEDTF + "/" + centuryToEDTF
-    
-    
-def singleYearWithQualifier(dateString):
-    yearSearch = re.search(r'(\d{4}\??)', dateString)
-    if not yearSearch:
-        return None
-    year = yearSearch.group(1)
-    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
-    if uncertain and not '?' in year:
-        return year + "?"
-    else:
-        return year
-
-def singleYearRelaxed(dateString):
-    return singleYearWithQualifier(dateString)
-
-def yearRangeWithQualifier(dateString):
-    years = re.findall(r'(\d{2,4}\??)', dateString)
-    uncertain = re.search(r'(ca)', dateString)
-    if uncertain:
-        for i, year in enumerate(years):
-            if not '?' in year:
-                years[i] += '?'
-    return "/".join(years)
-
-def singleDate(dateString):
-    date = re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', dateString)
-    if date:
-        return date.group(0)
-    else:
-        return None
 
 def fullDateWithMonthInLangOrRoman(dateString):
     allMonthsPattern = '|'.join(constants.ALLMONTHTERMS)
@@ -129,6 +74,30 @@ def fullDateWithMonthInLangOrRoman(dateString):
     
     return '.'.join([date, month, year])
 
+def guessMonth(monthString):
+    testOrder = ['de', 'en', 'fr', 'roman']
+    monthString = re.sub(r'\.|\s', '', monthString)
+    for lang in testOrder:
+        for i in constants.MONTHTERMS[lang].keys():
+            for monthVariation in constants.MONTHTERMS[lang][i]:
+                if monthVariation.lower() == monthString.lower():
+                    return i
+    return 0
+    
+def midCentury(dateString):
+    centurySearch = re.search(r'(\d)\.\s?[A-zäöü]*\s?(\d{1,2})', dateString)
+    if not centurySearch:
+        return None
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    half = centurySearch.group(1)
+    century = centurySearch.group(2)
+    centuryEDTF = str(int(century)-1)
+    qualifier = '?' if uncertain else ''
+    if half == "1":
+        return century + "00" + qualifier + "/" + century + "50" + qualifier
+    else:
+        return century + "50" + qualifier + "/" + century + "99" + qualifier
+
 def monthAndYearWithMonthInLangOrRoman(dateString):
     allMonthsPattern = '|'.join(constants.ALLMONTHTERMS)
     yearPattern = r'((\d{2,4})\.?$|\d{4})'
@@ -145,6 +114,27 @@ def monthAndYearWithMonthInLangOrRoman(dateString):
     
     return '.'.join([month, year])
 
+def singleDate(dateString):
+    date = re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', dateString)
+    if date:
+        return date.group(0)
+    else:
+        return None
+
+def singleYearRelaxed(dateString):
+    return singleYearWithQualifier(dateString)
+    
+def singleYearWithQualifier(dateString):
+    yearSearch = re.search(r'(\d{4}\??)', dateString)
+    if not yearSearch:
+        return None
+    year = yearSearch.group(1)
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    if uncertain and not '?' in year:
+        return year + "?"
+    else:
+        return year
+
 def yearWithPlaceHolderAndQualifier(dateString):
     uncertain = re.search(r'(ca|\?)', dateString)
     quantifier = '?' if uncertain else ''
@@ -156,3 +146,12 @@ def yearWithPlaceHolderAndQualifier(dateString):
     if m:
         century = m.group(1)
         return "%s0%s/%s9%s" % (century, quantifier, century, quantifier)
+
+def yearRangeWithQualifier(dateString):
+    years = re.findall(r'(\d{2,4}\??)', dateString)
+    uncertain = re.search(r'(ca)', dateString)
+    if uncertain:
+        for i, year in enumerate(years):
+            if not '?' in year:
+                years[i] += '?'
+    return "/".join(years)
