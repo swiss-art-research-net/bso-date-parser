ab10e4fc8abd253d65a6e17c50c27c7cb2fbc355
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-05 17:17:46 +0100

    initial commit

---
+++ bsoDateParser.py
@@ -0,0 +1,63 @@
+import re
+import sys
+
+import lib.constants as constants
+import lib.DateStringParsers as DateStringParsers
+import lib.TestPatterns as TestPatterns
+
+def cleanDateString(dateString):
+    s = re.sub('\[|\]', '', dateString)
+    return s
+
+def extractPattern(dateString):
+    # Remove square brackets that indicate deducted dates
+    genericDate = re.sub(r'\[|\]', '', dateString)
+
+    # Order of language preference for month detections
+    langOrder = ['de', 'en', 'fr']
+    # Normalise months in different languages
+    for lang in langOrder:
+        regexPattern = r'(' + ')|('.join([month for variations in [constants.MONTHTERMS[lang][d] for d in constants.MONTHTERMS[lang]] for month in variations]) + ')'                            
+        genericDate = re.sub(regexPattern, '🌕', genericDate, flags=re.IGNORECASE)
+    genericDate = re.sub(r'🌕r|🌕re|🌕s|🌕br|🌕st|🌕obr|🌕ob|🌕t', '🌕', genericDate, flags=re.IGNORECASE)
+
+    # Normalise indicators of unknown data
+    genericDate = re.sub(r'XX|xx', '❓', genericDate)
+
+    # Normalise month in roman numerals
+    monthsRoman = r'(' + ')|('.join([month for variations in [constants.MONTHTERMS['roman'][d] for d in constants.MONTHTERMS['roman']] for month in variations]) + ')'                            
+    genericDate = re.sub(monthsRoman, '🌕', genericDate)
+
+    # Normalise century terms
+    centuriesPattern = r'(' + ')|('.join(constants.ALLCENTURYTERMS) + ')'
+    genericDate = re.sub(centuriesPattern, '¢', genericDate)
+
+    # Normalise terms for half
+    midTermPattern = r'(' + ')|('.join(constants.ALLMIDTERMS) + ')'
+    genericDate = re.sub(midTermPattern, '½', genericDate)
+
+    # Normalise digits
+    genericDate = re.sub(r'\d','_', genericDate)
+
+    # Strip whitespace
+    genericDate = genericDate.strip()
+
+    return genericDate
+
+def interpret(dateString, pattern):
+    ds = cleanDateString(dateString)
+    testOrder = ['singleDate', 'fullDateWithMonthInLangOrRoman', 'monthAndYearWithMonthInLangOrRoman', 'singleYearWithQualifier', 'beforeYearWithQualifier', 'afterYearWithQualifier', 'yearRangeWithQualifier', 'yearWithPlaceHolderAndQualifier', 'centuryRange', 'midCentury', 'century', 'singleYearRelaxed']
+
+    for test in testOrder:
+        m = re.search(getattr(TestPatterns, test), pattern)
+        if m:    
+            f = getattr(DateStringParsers, test)
+            if not f:
+                raise NotImplementedError("Function %s not implemented" % test)
+            return f(ds)
+    
+    return None
+
+def parse(dateString):
+    pattern = extractPattern(dateString)
+    return interpret(dateString, pattern)
---
+++ lib/DateStringParsers.py
@@ -0,0 +1,158 @@
+import re
+
+import lib.constants as constants
+
+def guessMonth(monthString):
+    testOrder = ['de', 'en', 'fr', 'roman']
+    monthString = re.sub(r'\.|\s', '', monthString)
+    for lang in testOrder:
+        for i in constants.MONTHTERMS[lang].keys():
+            for monthVariation in constants.MONTHTERMS[lang][i]:
+                if monthVariation.lower() == monthString.lower():
+                    return i
+    return 0
+            
+def afterYearWithQualifier(dateString):
+    yearSearch = re.search(r'(\d{4}\??)', dateString)
+    if not yearSearch:
+        return None
+    year = yearSearch.group(1)
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    year = year + "/"
+    if uncertain and not '?' in year:
+        return year + "?"
+    else:
+        return year
+
+def beforeYearWithQualifier(dateString):
+    yearSearch = re.search(r'(\d{4}\??)', dateString)
+    if not yearSearch:
+        return None
+    year = yearSearch.group(1)
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    year = "/" + year
+    if uncertain and not '?' in year:
+        return year + "?"
+    else:
+        return year
+
+def century(dateString):
+    centurySearch = re.search(r'(\d{1,2})', dateString)
+    if not centurySearch:
+        return None
+    century = centurySearch.group(1)
+    centuryEDTF = str(int(century)-1) ## EDTF uses YY for century. 19th century is 18
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    if uncertain:
+        return centuryEDTF + "?"
+    else:
+        return centuryEDTF
+    
+def midCentury(dateString):
+    centurySearch = re.search(r'(\d)\.\s?[A-zäöü]*\s?(\d{1,2})', dateString)
+    if not centurySearch:
+        return None
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    half = centurySearch.group(1)
+    century = centurySearch.group(2)
+    centuryEDTF = str(int(century)-1)
+    qualifier = '?' if uncertain else ''
+    if half == "1":
+        return century + "00" + qualifier + "/" + century + "50" + qualifier
+    else:
+        return century + "50" + qualifier + "/" + century + "99" + qualifier
+
+def centuryRange(dateString):
+    centurySearch = re.findall(r'(\d{2})', dateString)
+    if len(centurySearch) <2:
+        return None
+    centuryFrom = centurySearch[0]
+    centuryTo = centurySearch[1]
+    centuryFromEDTF = str(int(centuryFrom)-1) ## EDTF uses YY for century. 19th century is 18
+    centuryToEDTF = str(int(centuryTo)-1) 
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    if uncertain:
+        return centuryFromEDTF + "?/" + centuryToEDTF + "?"
+    else:
+        return centuryFromEDTF + "/" + centuryToEDTF
+    
+    
+def singleYearWithQualifier(dateString):
+    yearSearch = re.search(r'(\d{4}\??)', dateString)
+    if not yearSearch:
+        return None
+    year = yearSearch.group(1)
+    uncertain = re.search(r'(' + constants.UNCERTAINTYQUALIFIERS + ')', dateString)
+    if uncertain and not '?' in year:
+        return year + "?"
+    else:
+        return year
+
+def singleYearRelaxed(dateString):
+    return singleYearWithQualifier(dateString)
+
+def yearRangeWithQualifier(dateString):
+    years = re.findall(r'(\d{2,4}\??)', dateString)
+    uncertain = re.search(r'(ca)', dateString)
+    if uncertain:
+        for i, year in enumerate(years):
+            if not '?' in year:
+                years[i] += '?'
+    return "/".join(years)
+
+def singleDate(dateString):
+    date = re.search(r'\d{1,2}\.\d{1,2}\.\d{2,4}', dateString)
+    if date:
+        return date.group(0)
+    else:
+        return None
+
+def fullDateWithMonthInLangOrRoman(dateString):
+    allMonthsPattern = '|'.join(constants.ALLMONTHTERMS)
+    datePattern = r'(\d{1,2})(?:\.|\s)*(?:' + allMonthsPattern + ')(?:\.|\s)*(?:\d{2,4})'
+    yearPattern = r'((\d{2,4})\.?$|\d{4})'
+    try:
+        date = re.search(datePattern, dateString, flags=re.IGNORECASE).group(1)
+    except:
+        return None
+        
+    try:
+        monthWords = re.search(allMonthsPattern, dateString, flags=re.IGNORECASE).group(0)
+        month = str(guessMonth(monthWords))
+    except:
+        return None
+
+    try:
+        year = re.search(yearPattern, dateString).group(1)
+    except:
+        return None
+    
+    return '.'.join([date, month, year])
+
+def monthAndYearWithMonthInLangOrRoman(dateString):
+    allMonthsPattern = '|'.join(constants.ALLMONTHTERMS)
+    yearPattern = r'((\d{2,4})\.?$|\d{4})'
+    try:
+        monthWords = re.search(allMonthsPattern, dateString, flags=re.IGNORECASE).group(0)
+        month = str(guessMonth(monthWords))
+    except:
+        return None
+
+    try:
+        year = re.search(yearPattern, dateString).group(1)
+    except:
+        return None
+    
+    return '.'.join([month, year])
+
+def yearWithPlaceHolderAndQualifier(dateString):
+    uncertain = re.search(r'(ca|\?)', dateString)
+    quantifier = '?' if uncertain else ''
+    m = re.search(r'(\d{2})--', dateString)
+    if m:
+        century = m.group(1)
+        return "%s00%s/%s99%s" % (century, quantifier, century, quantifier)
+    m = re.search(r'(\d{3})-', dateString)
+    if m:
+        century = m.group(1)
+        return "%s0%s/%s9%s" % (century, quantifier, century, quantifier)
