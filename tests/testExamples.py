3095a2ee04211fad66b92557bd1caea38919d5d8
Florian Kräutli <fkraeutli@mpiwg-berlin.mpg.de>
2020-11-16 14:10:39 +0100

    print summary

--- tests/testExamples.py
+++ tests/testExamples.py
@@ -14,8 +14,12 @@ with open(examplesFile, 'r') as f:
     for row in reader:
         examples.append(row)
 
+countErrors = 0
 for i, example in enumerate(examples):
     try:
         assert parse(example['input']) == example['output'], example['input'] + " should be " + example['output']
     except AssertionError: 
+        countErrors +=1
         sys.stderr.write("%d: %s is %s instead of %s\n" % (i, example['input'], str(parse(example['input'])), example['output']))
+
+print("Completed with %d out of %d tests failed" % (countErrors, len(examples)))
